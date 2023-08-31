9c06623b2c8c4b230eaf939e99f08e681e895af7
hmz4dev <hmz4dev@gmail.com>
2023-08-31 19:22:27 +0100

    modif 31/08/23

--- resources/views/components/commande/tabs/lignes.blade.php
+++ resources/views/components/commande/tabs/lignes.blade.php
@@ -1,13 +1,10 @@
    @can('create', [App\Models\Lcommande::class, $commande])
        <div class="ui mini icon right floated load-model button"
-           style="background-color:rgba(202, 228, 246, 0.874) !important; ; color: #000"
+           style="background-color:rgba(202, 228, 246, 0.874) !important; ; color: #000; margin-bottom:7px"
            data-url="{{ Route('commande.ligne.create', [$commande->id]) }}?&"
            data-title="<img src='{{ asset('assets/images/app_logo_sq.png') }}' height='20px' /> <span style='vertical-align: super;color:#000'>&nbsp;Ajouter Nouvel article</span>"
            data-color="rgba(202, 228, 246, 0.874) none repeat scroll 0% 0%" data-width="850"><i
                class="add icon"></i>&nbsp;Ajouter nouvel article</div>
    @endcan
-
-   <br>
-   <br>
    <x-data-table list="lcommandes" :vdata="$vdata" length="-1" :paging="false" classes="child" order="0"
        appends="commande_id={{ $commande->id }}" />
